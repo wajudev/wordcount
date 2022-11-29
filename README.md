0b285efc9849f652e2d4d34b4160317f33ff4dcb
Waju <lanre.ajibua@gmail.com>
2022-11-29 12:52:18 +0100

    recursive_directory_search inkl. file extensions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # wordcount
+
+This application given a path to a directory on the disk, computes counts the 
+occurences of all words and prints them accordingly.
--- main.cpp
+++ main.cpp
@@ -2,25 +2,29 @@
 #include <string>
 #include <filesystem>
 
-auto recursive_directory_search =[](const std::string &path){
+auto recursive_directory_search =[](const std::string &path, const std::string &extension){
     for (const auto & entry: std::filesystem::recursive_directory_iterator(path)) {
         // TODO Do something cool other than print
-        std::cout << entry.path() << std::endl;
+        if (entry.path().extension() == extension){
+            std::cout << entry.path().stem().string() << std::endl;
+        }
     }
 };
 
-// TODO Figure out how to integrate file extension as second parameter
+
 // TODO Figure out how to access content of file
 // TODO Word counter
 // TODO Word printer
 
 
 int main(){
-    std::string path;
+    std::string path, extension;
     std::cout << "Enter directory path" << std::endl;
     std::cin >> path;
+    std::cout << "Enter extension" << std::endl;
+    std::cin >> extension;
 
-    recursive_directory_search(path);
+    recursive_directory_search(path, extension);
 
     std::cout << "Hello, Welcome to Word Count Universe, where we count words pointlessly" << std::endl;
     return 0;
