0a4e96f2f148938fb9743ff5cacab049b7542836
Waju <lanre.ajibua@gmail.com>
2022-11-30 12:14:00 +0100

    added filepath to vector

--- main.cpp
+++ main.cpp
@@ -3,10 +3,22 @@
 #include <fstream>
 #include <map>
 #include <filesystem>
+#include <vector>
 
+
+
+// TODO: Sort function - decreasing order -> most common to least common -> how to sort the second value of a map? is it better to sort an unordered map!
+// TODO: Handle human errors etc.
+// TODO: Tests script for program
+// TODO: Identify side effects of each function
+// TODO: Highlight functional and non functional part of programm
+// TODO: Powerpoint presentation
+// TODO: READMe update -> containing how to use and start programm
+// TODO: Add debug to makefile maybe?
 
 // store string/int pairs
 typedef std::map<std::string, int> StringIntMap;
+
 
 // TODO: make word counter conform to functional programming paradigm -> replace while loop maybe?
 auto word_counter = [](std::istream &input, StringIntMap &words) {
@@ -21,36 +33,43 @@ auto word_counter = [](std::istream &input, StringIntMap &words) {
 };
 
 auto recursive_directory_search = [](const std::string &path, const std::string &extension) {
-    std::string filepath;
+    std::vector <std::string> filepath;
     for (const auto &entry: std::filesystem::recursive_directory_iterator(path)) {
-        // TODO Do something cool other than print
         if (entry.path().extension() == extension) {
-            // TODO maybe save all paths in a vector to be accessed later (Accessed recursively?)
-            // std::cout << entry.path().stem().string() << std::endl;
-            filepath = entry.path();
+            //std::cout << entry.path().stem().string() << std::endl;
+            filepath.push_back(entry.path());
+            /*for (auto i: filepath) {
+                std::cout << i << std::endl;
+            }*/
         }
     }
     return filepath;
 };
 
-// TODO: Figure out how to access content of all files -> currently only works for the last file foun
-auto file_accessor = [](const std::string &extensionFile) {
-    std::ifstream file;
+/*auto map_sorter = [](const StringIntMap &map) {
+
+};*/
+
+
+// TODO: Maybe break down into multiple functions -
+auto file_accessor = [](const std::vector <std::string> &extensionFile) {
+    std::ifstream inputFile;
     StringIntMap map;
 
-    file.open(extensionFile, std::ios::out);
-
-    if (file.is_open()) {
-        word_counter(file, map);
-        for (auto &word: map) {
-            // feature of map that uses <utility> template
-            std::cout << word.first << " -> " << word.second << std::endl;
-        }
-        file.close();
-    } else std::cout << "Unable to open file" << std::endl;
+    for (auto file: extensionFile) {
+        inputFile.open(file, std::ios::out);
+        if (inputFile.is_open()) {
+            word_counter(inputFile, map);
+            for (auto &word: map) {
+                // feature of map that uses <utility> template
+                std::cout << word.first << " -> " << word.second << std::endl;
+                //map_sorter(word);
+            }
+            inputFile.close();
+        } else std::cout << "Unable to open file" << std::endl;
+    }
 };
 
-// TODO: Handle human errors etc.
 
 int main() {
     std::string path, extension;
@@ -59,6 +78,7 @@ int main() {
     std::cout << "Enter extension" << std::endl;
     std::cin >> extension;
 
+    //recursive_directory_search(path, extension);
     file_accessor(recursive_directory_search(path, extension));
 
     return 0;
